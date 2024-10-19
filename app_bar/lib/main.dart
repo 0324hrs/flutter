ed44d6ff7c11f81d9bc04dfe44faa38199b70985
unknown <sct2540042020@mmu.ac.ke>
2024-10-19 04:18:12 +0300

    first commit

---
+++ app_bar/lib/main.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.blue,
+          elevation: 0,
+
+          /*  leading: IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.menu),
+            color: Colors.white,
+          ), */
+          title: Text(
+            "Facebook",
+            style: TextStyle(color: Colors.white),
+          ),
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.search),
+              color: Colors.white,
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.menu),
+              color: Colors.white,
+            ),
+          ],
+
+          //leading: IconButton(onPressed: () {}, icon: Icon(Icons.menu)),
+        ),
+      ),
+      //title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      //home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
